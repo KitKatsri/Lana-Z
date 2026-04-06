9dc22f49706159f0fe953465c4cc4813380e6ad4
KitKatsri <123915617+KitKatsri@users.noreply.github.com>
2026-04-06 16:28:04 +0530

    Create main app structure with routing and theme

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'routes.dart';
+import 'theme/app_theme.dart';
+import 'screens/splash_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/player_screen.dart';
+
+class LanaZApp extends StatelessWidget {
+  const LanaZApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lana Z',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkTheme,
+      initialRoute: AppRoutes.splash,
+      routes: {
+        AppRoutes.splash: (_) => const SplashScreen(),
+        AppRoutes.home: (_) => const HomeScreen(),
+        AppRoutes.player: (_) => const PlayerScreen(),
+      },
+    );
+  }
+}
